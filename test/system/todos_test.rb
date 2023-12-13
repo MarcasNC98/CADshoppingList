273b2c8406f201459ffa54e0de3a5402d4de2d2c
Mark Cummins <cumminsmark98@gmail.com>
2023-12-13 23:18:11 +0000

    Updated system tests

--- test/system/todos_test.rb
+++ test/system/todos_test.rb
@@ -9,7 +9,7 @@ class TodosTest < ApplicationSystemTestCase
     visit todos_url
     assert_selector "h1", text: "Items"
   end
-
+  
   test "should create Item" do
     visit todos_url
     click_on "New item"
@@ -18,7 +18,9 @@ class TodosTest < ApplicationSystemTestCase
     check "Purchased" if @todo.purchased
     click_on "Create item"
 
-    assert_text "Item was successfully created."
+    # Because of the validation logic that prevents duplicate data being saved, test will check if this functionality works correctly
+    assert_text "1 error prohibited this item from being saved:"
+    assert_text "Item is already on the list"
     click_on "Back"
   end
 
@@ -28,15 +30,18 @@ class TodosTest < ApplicationSystemTestCase
 
     fill_in "Item", with: @todo.item
     check "Purchased" if @todo.purchased
-    click_on "Update item"
+    click_on "Create item"
 
-    assert_text "Item was successfully updated."
-    click_on "Back"
+    assert_text "1 error prohibited this item from being saved:"
+    assert_text "Item is already on the list"
   end
 
   test "should destroy Item" do
     visit todo_url(@todo)
     click_on "Delete this item", match: :first
+    accept_confirm do
+      # Will click 'ok' on the alert
+    end
 
     assert_text "Item was successfully deleted."
   end
