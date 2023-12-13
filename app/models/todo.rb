3bd4ccf47e3b4952c88dc4e1fe015090bd62b5d6
Mark Cummins <cumminsmark98@gmail.com>
2023-12-13 18:00:35 +0000

    Comments
    Added links to documentation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -8,9 +8,9 @@ class Todo < ApplicationRecord
         end
     end
 
-    # has_one_attached allows images to be shown in 'show.html.erb' by setting up a map betwen an items record and a file
+    # has_one_attached allows images to be shown in 'show.html.erb' by setting up a map betwen an items record and a file https://edgeguides.rubyonrails.org/active_storage_overview.html#has-one-attached 
     has_one_attached :image
 
-    # Validation that prevents an item from being added if the same item is already on the list or if the field is left blank
+    # Validation that prevents an item from being added if the same item is already on the list or if the field is left blank https://guides.rubyonrails.org/active_record_validations.html 
     validates :item, presence: true, uniqueness: { message: " is already on the list" }
 end
