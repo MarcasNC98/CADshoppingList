b491892386266210c2fe95c338d98d2c0bfd31c8
Mark Cummins <cumminsmark98@gmail.com>
2023-12-13 15:27:58 +0000

    Comments

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -11,6 +11,6 @@ class Todo < ApplicationRecord
     # has_one_attached allows images to be shown in 'show.html.erb' by setting up a map betwen an items record and a file
     has_one_attached :image
 
-    # Validation that prevents an item from being added if the same item is already on the list
+    # Validation that prevents an item from being added if the same item is already on the list or if the field is left blank
     validates :item, presence: true, uniqueness: { message: "Item is already on the list" }
 end
